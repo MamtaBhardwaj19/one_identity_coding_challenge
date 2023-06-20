90ed7de9fc9885caa46ed44716eacd0940de101d
Mamta Bhardwaj <mamta.bhardwaj@mitratech.com>
2023-06-20 12:48:43 +0530

    Add comments and README.md file

---
+++ README.md
@@ -0,0 +1,11 @@
+== README
+
+This is a command-line program that takes operations on fractions as input and produces a fractional result.
+
+## Steps to run
+            $ git clone git@github.com:MamtaBhardwaj19/one_identity_coding_challenge.git
+            $ bundle install
+            $ ruby solution.rb
+
+## Tests
+            $ rspec
--- lib/calculator.rb
+++ lib/calculator.rb
@@ -11,6 +11,7 @@ class Calculator
 
   def result
     operand1, operator, operand2 = equation&.split(OPERATORS_REGEX)&.map { |num| simplify_mixed_number(num) }
+    # User should input operand1, legal operator and operand2, otherwise return with a message.
     return 'Incorrect Equation' if [operand1, operator, operand2].compact.size < 3 || !OPERATORS.include?(operator)
 
     res = case operator
@@ -55,13 +56,16 @@ class Calculator
     "#{(first_num.to_i * denominator.to_i) + numerator.to_i}/#{denominator}"
   end
 
+  # return res if it is a whole number or
+  # mixed number if it is an improper fraction,
   def to_mixed_number(res)
-    return res.to_s unless res.to_s.include?('/')
+    res = res.to_s
+    return res unless res.include?('/')
 
-    numerator, denominator = res.to_s.split('/').map(&:to_i)
+    numerator, denominator = res.split('/').map(&:to_i)
 
     return numerator.to_s if denominator.to_i == 1
-    return res.to_s if numerator < denominator
+    return res if numerator < denominator
 
     first_num = numerator / denominator
     numerator = numerator % denominator
